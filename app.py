7dac84e5e2d416411d164643efe0e8510bc6ae84
Alejandro <23301224@uttt.edu.mx>
2026-09-25 12:37:12 -0600

    Feat: Agregar ruta api status con conflicto

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask
 
 nombre = "Alejandro"
 
-app = Flask("app_error")
+app = Flask("app_conflicto")
 
 @app.route('/')
 def index():
-    return f"
+    return f"<h1>Bienvenido al portal universitario, {nombre}!</h1>"
 
-Bienvenido al portal universitario, {nombre}!
-"
+@app.route('/api/status')
+def status():
+    return {"status": "ok", "entorno": "contenedor-docker", "version": "1.1.0"}
 
-if name == 'main':
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
