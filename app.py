bf28e2788ce8e201d33d10b306c894fe3635419e
Alejandro <23301224@uttt.edu.mx>
2026-09-25 12:15:28 -0600

    Feat: Agregar endpoint /api/status

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from flask import Flask
 # Nombre a desplegar
 nombre = "Alejandro"
 
-app = Flask_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return f"
 Bienvenido al portal universitario, {nombre}!
 "
+
+@app.route('/api/status')
+def status():
+return {"status": "ok", "entorno": "contenedor-docker", "version": "1.1.0"}
 
 if name == 'main':
 app.run(host='0.0.0.0', port=5000)
