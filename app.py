2f07b34187cef982486ed6d3fe798742ea3fc113
Alejandro <23301224@uttt.edu.mx>
2026-09-25 11:56:53 -0600

    Initial commit - Flask app con Dockerfile

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+# Nombre a desplegar
+nombre = "Alejandro"
+
+app = Flask_name_)
+
+@app.route('/')
+def index():
+    return f"
+Bienvenido al portal universitario, {nombre}!
+"
+
+if name == 'main':
+app.run(host='0.0.0.0', port=5000)
